37cff28c9821bc4ab0e7c4179452b234ef0b47c9
js829 <js829@njit.edu>
2017-02-04 15:45:57 -0500

    add post arrays

--- display_discount.php
+++ display_discount.php
@@ -1,9 +1,9 @@
 <?php
 
    // get the data from the form
-   $product_description = $_POST['product_description'];
-   $list_price = $_POST['list_price'];
-   $discount_percent = $_POST['discount_percent'];
+   $product_description = filter_input(INPUT_POST, 'product_description');
+   $list_price = filter_input(INPUT_POST, 'list_price');
+   $discount_percent = filter_input (INPUT_POST, 'discount_percent');
 
    // calculate the discount and discounted price
    $discount = $list_price * $discount_percent * .01;
@@ -14,10 +14,7 @@
    $discount_percent_f = $discount_percent."%";
    $discont_f = "$".number_format($discount, 2);
    $discount_price_f = "$".number_format($discount_price, 2);
-
 ?>
-
-
 <!DOCTYPE html>
 <html>
 
@@ -31,29 +28,24 @@
        <h1>Product Discount Calculator</h1>
 
        <label>Product Description:</label>
-       <span><?php echo htmlspecialchars(
-                   $product_description); ?></span>
+       <span><?php echo htmlspecialchars($product_description); ?></span>
        <br>
 
        <label>List Price:</label>
-       <span><?php echo htmlspecialchars(
-       		   $list_price_f); ?></span>
+       <span><?php echo htmlspecialchars($list_price_f); ?></span>
+       <br>
 
-	<br>
+       <label>Standard Discount:</label>
+       <span><?php echo htmlspecialchar($discount_percent_f); ?></span>
+       <br>
 
-	<label>Standard Discount:</label>
-	<span><?php echo htmlspecialchar( 
-	                   $discount_percent_f); ?></span>
-	<br>
+       <label>Discount Amount:</label>
+       <span><?php echo $discount_f; ?></span>
+       <br>
 
-	<label>Discount Amount:</label>
-	<span><?php echo $discount_f; ?></span>
-	<br>
-
-	<label>Discount Price:</label>
-	<span><?php echo $discount_price_f; ?></span>
-	<br>
+       <label>Discount Price:</label>
+       <span><?php echo $discount_price_f; ?></span>
+       <br>
    </main>
 </body>
 </html>
-
